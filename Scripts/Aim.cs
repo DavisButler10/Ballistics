c74da0ee8d73d05fb59af0be27712a2985d0e779
DavisButler10 <45404502+DavisButler10@users.noreply.github.com>
2020-02-27 20:17:45 -0700

    Add files via upload

---
+++ Scripts/Aim.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour
+{
+    public float myTimeScale = 10.0f;
+    public GameObject target;
+    public float launchForce = 10f;
+    Rigidbody rb;
+
+    void Start()
+    {
+        Time.timeScale = myTimeScale; 
+        rb = GetComponent<Rigidbody>();
+
+        FiringSolution fs = new FiringSolution();
+        Nullable<Vector3> aimVector = fs.Calculate(transform.position, target.transform.position, launchForce, Physics.gravity);
+
+        if (aimVector.HasValue)
+        {
+            rb.AddForce(aimVector.Value.normalized * launchForce, ForceMode.VelocityChange);
+        }
+    }
+}
